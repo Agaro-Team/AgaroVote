705bd41f51dfa6d63fa3595b53159e0ebaa56a8d
Rachmizard <rachmizard11072000@gmail.com>
2025-10-06 17:02:08 +0700

    feat(AGARO-MAIN-FE): refactor wagmi configuration to support dynamic plugin loading based on environment. Enhanced configuration to include hardhat plugin for development with specific contract addresses, improving flexibility for different deployment scenarios.

--- AGARO-MAIN-FE/wagmi.config.ts
+++ AGARO-MAIN-FE/wagmi.config.ts
@@ -1,22 +1,31 @@
-import { counterConfig } from '~/lib/web3/contracts/generated';
+import type { Address } from 'viem';
 
-import { defineConfig } from '@wagmi/cli';
-import { react } from '@wagmi/cli/plugins';
+import { type Plugin, defineConfig, loadEnv } from '@wagmi/cli';
+import { hardhat, react } from '@wagmi/cli/plugins';
 
-export default defineConfig({
-  out: 'app/lib/web3/contracts/generated.ts',
-  contracts: [
-    {
-      name: 'Counter',
-      abi: counterConfig.abi,
-      address: counterConfig.address,
-    },
-  ],
+export default defineConfig(async () => {
+  const env = loadEnv({
+    mode: process.env.NODE_ENV,
+    envDir: process.cwd(),
+  });
 
-  plugins: [
-    react(),
-    // hardhat({
-    //   project: '../AGARO-CONTRACT',
-    // }),
-  ],
+  const plugins: Plugin[] = [react()];
+
+  if (process.env.NODE_ENV === 'dev') {
+    plugins.push(
+      hardhat({
+        project: '../AGARO-CONTRACT',
+        deployments: {
+          Counter: {
+            '31337': env.AGARO_VOTE_CONTRACT_ADDRESS_HARDHAT as Address,
+          },
+        },
+      })
+    );
+  }
+
+  return {
+    out: 'app/lib/web3/contracts/generated.ts',
+    plugins,
+  };
 });
