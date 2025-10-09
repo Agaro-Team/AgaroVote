90d8a10f6e614af2e21c452b04b74589a37c5eeb
Rachmizard <rachmizard11072000@gmail.com>
2025-10-09 21:36:17 +0700

    chore(AGARO-MAIN-FE): comment out unused script in root component for improved maintainability

--- AGARO-MAIN-FE/app/root.tsx
+++ AGARO-MAIN-FE/app/root.tsx
@@ -37,7 +37,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="" suppressHydrationWarning>
       <head>
-        <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+        {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" /> */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
