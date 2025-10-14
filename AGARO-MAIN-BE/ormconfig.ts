576ac435f4abefd30971ce9d055e77021b926f00
Rachmizard <rachmizard11072000@gmail.com>
2025-10-15 01:28:15 +0700

    chore(Dockerfile, ormconfig, package.json): update production migration commands and database configuration for improved environment handling

--- AGARO-MAIN-BE/ormconfig.ts
+++ AGARO-MAIN-BE/ormconfig.ts
@@ -3,6 +3,8 @@ import { config } from 'dotenv';
 
 config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
@@ -10,7 +12,11 @@ export default new DataSource({
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'agaro_vote_db',
-  entities: ['src/**/*.entity{.ts,.js}'],
-  migrations: ['src/database/migrations/**/*{.ts,.js}'],
+  entities: isProduction
+    ? ['dist/src/**/*.entity.js']
+    : ['src/**/*.entity{.ts,.js}'],
+  migrations: isProduction
+    ? ['dist/src/database/migrations/**/*.js']
+    : ['src/database/migrations/**/*{.ts,.js}'],
   synchronize: false,
 });
