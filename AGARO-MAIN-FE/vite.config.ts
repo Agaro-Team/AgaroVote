9101668ed310212ef18cd4abdf70c97238057263
Rachmizard <rachmizard11072000@gmail.com>
2025-10-18 11:23:13 +0700

    feat(netlify): add Netlify configuration for build and development; include netlify-plugin-react-router in dependencies and update Vite configuration

--- AGARO-MAIN-FE/vite.config.ts
+++ AGARO-MAIN-FE/vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { reactRouter } from '@react-router/dev/vite';
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
+
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
