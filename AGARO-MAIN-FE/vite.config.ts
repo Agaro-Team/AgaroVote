5de84bf582f81859735a1116da927736257f9661
Rachmizard <rachmizard11072000@gmail.com>
2025-10-19 12:54:30 +0700

    chore(vite): remove Netlify plugin from Vite configuration to streamline plugin usage

--- AGARO-MAIN-FE/vite.config.ts
+++ AGARO-MAIN-FE/vite.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { reactRouter } from '@react-router/dev/vite';
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
-
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
