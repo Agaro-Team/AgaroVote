de617123d616739307a52e427f6633afab54382b
Gifar Ananda Maulana <giffaranandaa@gmail.com>
2025-10-21 13:42:19 +0700

    feat(layout): integrate LazyMotion for enhanced animations in MainLayout component

--- AGARO-LANDING-PAGE-FE/src/Layouts/MainLayout.tsx
+++ AGARO-LANDING-PAGE-FE/src/Layouts/MainLayout.tsx
@@ -2,13 +2,16 @@ import { Outlet } from 'react-router-dom';
 import Header from '../Layouts/Header';
 import Footer from './Footer';
 import { ThemeProvider } from '../lib/theme-provider';
+import { domAnimation, LazyMotion } from 'motion/react';
 
 const MainLayout = () => {
   return (
     <ThemeProvider>
       <div className="flex flex-col min-h-screen relative bg-[var(--background)] text-[var(--foreground)] transition-colors duration-300">
         <Header />
-        <Outlet />
+        <LazyMotion features={domAnimation}>
+          <Outlet />
+        </LazyMotion>
         <Footer />
       </div>
     </ThemeProvider>
